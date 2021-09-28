23695f446dec9401b15041e5541f29d31591a7ad
Dennis <dennis.lercher@gmx.de>
2021-09-29 00:56:33 +0200

    implement error handling
    also test the errors

--- CodingChallange/merge.py
+++ CodingChallange/merge.py
@@ -1,6 +1,13 @@
 from typing import Generator
 
 def merge(intervals: list) -> Generator[list, None, None]:
+    # Validate intervals / Fail fast
+    if type(intervals) != list:
+        raise ValueError('Argument is not a list of intervals')
+    # map reduce memory usage compared with a loop
+    list(map(validate_interval, intervals))
+
+    # sort on a list of lists will sort by the first index of each list element
     intervals.sort()
 
     if len(intervals):
@@ -14,3 +21,13 @@ def merge(intervals: list) -> Generator[list, None, None]:
                 start = interval[0]
                 end = interval[1]
         yield [start, end]
+
+def validate_interval(interval: list) -> None:
+    if type(interval) != list:
+        raise ValueError('Interval is not a list')
+    if len(interval) != 2:
+        raise ValueError('Interval should exist of 2 elements')
+    if type(interval[0]) != int:
+        raise ValueError('First element of interval is not an integer')
+    if type(interval[1]) != int:
+        raise ValueError('Second element of interval is not an integer')
--- tests/test_CodingChallange/test_merge.py
+++ tests/test_CodingChallange/test_merge.py
@@ -1,3 +1,4 @@
+import pytest
 from CodingChallange.merge import merge
 
 def test_merge_example() -> None:
@@ -5,13 +6,13 @@ def test_merge_example() -> None:
     intervals = [[25,30],[2,19],[14, 23],[4,8]]
     assert list(merge(intervals)) == [[2,23],[25,30]]
 
-def test_merge_nothing_to_merge_ascending_order():
+def test_merge_nothing_to_merge_ascending_order() -> None:
     # This will create a list of intervals with nothing to merge sorted in ascending order
     # [[1, 2], [3, 4], ..., [99, 100]]
     intervals = [[x, x+1] for x in range(1, 100, 2)]
     assert list(merge(intervals)) == [[x, x+1] for x in range(1, 100, 2)]
 
-def test_merge_nothing_to_merge_descending_order():
+def test_merge_nothing_to_merge_descending_order() -> None:
     # This will create a list of intervals with nothing to merge sorted in descending order
     # [[99, 100], [97, 98], ..., [1, 2]]
     intervals = [[x-1, x] for x in range(100, 0, -2)]
@@ -32,6 +33,60 @@ def test_merge_maximun_overlapping_intervals() -> None:
     intervals = [[x, int(half_intervals_size + x)] for x in range(1, int(half_intervals_size))]
     assert list(merge(intervals)) == [[1,intervals_size-1]]
 
-def test_merge_empty_intervals():
+def test_merge_empty_intervals() -> None:
     intervals = []
     assert list(merge(intervals)) == []
+
+def test_merge_non_list_of_intervals() -> None:
+    intervals = 'foo'
+    with pytest.raises(ValueError) as excinfo:
+        list(merge(intervals))
+    assert 'Argument is not a list of intervals' in str(excinfo.value)
+
+def test_merge_list_of_string() -> None:
+    intervals = ['foo']
+    with pytest.raises(ValueError) as excinfo:
+        list(merge(intervals))
+    assert 'Interval is not a list' in str(excinfo.value)
+
+def test_merge_string_in_first_interval_start() -> None:
+    intervals = [['bar', 1]]
+    with pytest.raises(ValueError) as excinfo:
+        list(merge(intervals))
+    assert 'First element of interval is not an integer' in str(excinfo.value)
+
+def test_merge_string_in_first_interval_end() -> None:
+    intervals = [[1, 'bar']]
+    with pytest.raises(ValueError) as excinfo:
+        list(merge(intervals))
+    assert 'Second element of interval is not an integer' in str(excinfo.value)
+
+def test_merge_string_in_second_interval_start() -> None:
+    intervals = [[1,2],['bar',4]]
+    with pytest.raises(ValueError) as excinfo:
+        list(merge(intervals))
+    assert 'First element of interval is not an integer' in str(excinfo.value)
+
+def test_merge_string_in_second_interval_end() -> None:
+    intervals = [[1,2],[3,'bar']]
+    with pytest.raises(ValueError) as excinfo:
+        list(merge(intervals))
+    assert 'Second element of interval is not an integer' in str(excinfo.value)
+
+def test_merge_empty_interval() -> None:
+    intervals = [[]]
+    with pytest.raises(ValueError) as excinfo:
+        list(merge(intervals))
+    assert 'Interval should exist of 2 elements' in str(excinfo.value)
+
+def test_merge_less_than_2_elements_in_interval() -> None:
+    intervals = [[1]]
+    with pytest.raises(ValueError) as excinfo:
+        list(merge(intervals))
+    assert 'Interval should exist of 2 elements' in str(excinfo.value)
+
+def test_merge_more_than_2_elements_in_interval() -> None:
+    intervals = [[1,2,3]]
+    with pytest.raises(ValueError) as excinfo:
+        list(merge(intervals))
+    assert 'Interval should exist of 2 elements' in str(excinfo.value)
