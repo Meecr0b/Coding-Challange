0a38db5fb193ba075e5f1f9b671a70209d3d20b5
Dennis <dennis.lercher@gmx.de>
2021-09-28 20:53:00 +0200

    implement basic merge function
    add 4 new tests:
      - test with nothing to merge ascending order
      - test with nothing to merge descending order
      - test large intervals in two ways

--- CodingChallange/merge.py
+++ CodingChallange/merge.py
@@ -1,4 +1,16 @@
 from typing import Generator
 
 def merge(intervals: list) -> list:
-    return [[2,23],[25,30]]
+    response = []
+    intervals.sort()
+    start, end = intervals[0]
+    for start_n, end_n in intervals[1:]:
+        if start_n <= end:
+            if end_n > end:
+                end = end_n
+        else:
+            response.append([start, end])
+            start = start_n
+            end = end_n
+    response.append([start, end])
+    return response
--- tests/test_CodingChallange/test_merge.py
+++ tests/test_CodingChallange/test_merge.py
@@ -1,5 +1,33 @@
 from CodingChallange.merge import merge
 
 def test_merge_example() -> None:
+    # The expected result from the example provided
     intervals = [[25,30],[2,19],[14, 23],[4,8]]
     assert list(merge(intervals)) == [[2,23],[25,30]]
+
+def test_merge_nothing_to_merge_ascending_order():
+    # This will create a list of intervals with nothing to merge sorted in ascending order
+    # [[1, 2], [3, 4], ..., [99, 100]]
+    intervals = [[x, x+1] for x in range(1, 100, 2)]
+    assert list(merge(intervals)) == [[x, x+1] for x in range(1, 100, 2)]
+
+def test_merge_nothing_to_merge_descending_order():
+    # This will create a list of intervals with nothing to merge sorted in descending order
+    # [[99, 100], [97, 98], ..., [1, 2]]
+    intervals = [[x-1, x] for x in range(100, 0, -2)]
+    assert list(merge(intervals)) == [[x, x+1] for x in range(1, 100, 2)]
+
+def test_merge_one_big_interval() -> None:
+    # This will create a list of intervals, where the first element already includes all other intervals
+    # [[1, 29999999], [2, 29999998], ....]
+    intervals_size = 30000000
+    intervals = [[x, intervals_size - x] for x in range(1, int(intervals_size/2))]
+    assert list(merge(intervals)) == [[1,intervals_size-1]]
+
+def test_merge_maximun_overlapping_intervals() -> None:
+    # This will create a list of intervals, where every element overlaps by one
+    # [[1, 15000001], [2, 15000002], ....]
+    intervals_size = 30000000
+    half_intervals_size = intervals_size/2
+    intervals = [[x, int(half_intervals_size + x)] for x in range(1, int(half_intervals_size))]
+    assert list(merge(intervals)) == [[1,intervals_size-1]]
